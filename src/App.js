2a163b200ec3ea2c3b0b6e3a38056c14e8f55c0c
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2021-01-15 00:08:37 -0600

    added react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import  seedColors from './seedColors';
 import Palette from './Palette';
 import { generatePalette } from './colorHelpers';
@@ -7,9 +8,13 @@ class App extends Component {
   render() {
     // console.log(generatePalette(seedColors[4]));
     return (
-      <div>
-        <Palette palette={generatePalette(seedColors[4])}/>
-      </div>
+      <Switch>
+        <Route exact path='/' render={() => <h1>List of Color Palettes here</h1>}/>
+        <Route exact path='/palette/:id' render={() => <h1>List of Individual Color Palette here</h1>}/>
+      </Switch>
+      // <div>
+      //   <Palette palette={generatePalette(seedColors[4])}/>
+      // </div>
     )
   }
 }
