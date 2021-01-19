099bec8342db2decd855321a4c4d3580157a2a85
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2021-01-18 20:09:27 -0600

    added link to single palette

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
         <Route 
           exact 
           path='/' 
-          render={() => <PaletteList palettes={seedColors}/>}
+          render={(routerProps) => <PaletteList palettes={seedColors} {...routerProps}/>}
         />
         <Route 
           exact 
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -35,8 +35,12 @@ const styles = {
 }
 
 class PaletteList extends Component {
+  goToPalette = (id) => {
+    this.props.history.push(`/palette/${id}`);
+  }
   render() {
     const { palettes, classes } = this.props;
+    console.log(this.props);
     return (
       <div className={classes.root}>
         <div className={classes.container}>
@@ -44,7 +48,7 @@ class PaletteList extends Component {
             <h1>React Colors</h1>
           </nav>
           <div className={classes.palettes}>
-            {palettes.map(palette => <MiniPalette {...palette} />)}
+            {palettes.map(palette => <MiniPalette {...palette} handleClick={() => this.goToPalette(palette.id)}/>)}
           </div>
         </div>
       </div>
