9d13a60df8a6692a29ce3baeccc85de13cff618d
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2021-01-18 18:23:55 -0600

    started styling MiniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -7,13 +7,7 @@ export default class PaletteList extends Component {
     const { palettes } = this.props;
     return (
       <div>
-        <MiniPalette />
-        {palettes.map(palette => (
-          <p>
-            <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-          </p>)
-          )
-        }
+        {palettes.map(palette => <MiniPalette {...palette} />)}
       </div>
     )
   }
