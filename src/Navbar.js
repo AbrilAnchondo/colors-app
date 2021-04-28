01fc1c29f29f20f9622e7942ab4843c620c5ffb6
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2021-04-28 18:52:58 -0400

    refactored Navbar Styles and deleted .css files

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
-import './Navbar.css';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
+import Slider from 'rc-slider';
 
-export default class Navbar extends Component {
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles/NavbarStyles';
+
+class Navbar extends Component {
   state = {
     format: 'hex',
     open: false
@@ -26,18 +28,18 @@ export default class Navbar extends Component {
   }
 
   render() {
-    const { level, changeColorLevel, showAllColors } = this.props;
+    const { level, changeColorLevel, showAllColors, classes } = this.props;
     const { format } = this.state;
     return (
-      <header className='Navbar'>
-        <div className='logo'>
+      <header className={classes.Navbar}>
+        <div className={classes.logo}>
           <Link to='/'>ReactColorPicker</Link>
         </div>
         {
           showAllColors && 
-            <div className='slider-container'>
+            <div>
               <span>Level: {level}</span>
-              <div className='slider'>
+              <div className={classes.slider}>
                 <Slider 
                   defaultValue={level}
                   min={100}
@@ -48,7 +50,7 @@ export default class Navbar extends Component {
               </div>
             </div>
         }
-        <div className='select-container'>
+        <div className={classes.selectContainer}>
           <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value='hex'>HEX - #ffffff</MenuItem>
             <MenuItem value='rgb'>RGB - rgb(255, 255, 255)</MenuItem>
@@ -81,3 +83,5 @@ export default class Navbar extends Component {
     )
   }
 }
+
+export default withStyles(styles)(Navbar);
---
+++ src/styles/NavbarStyles.js
@@ -0,0 +1,49 @@
+export default {
+  Navbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    height: "6vh"
+  },
+  
+  logo: {
+    marginRight: "15px",
+    backgroundColor: "#ececff",
+    fontSize: "22px",
+    padding: "0 13px",
+    fontFamily: "'Roboto', sans-serif",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    "& a": {
+      textDecoration: "none",
+      color: "black"
+    }
+  },
+  
+  slider: {
+    margin: "0 10px",
+    width: "340px",
+    display: "inline-block",
+    "& .rc-slider-track": {
+      backgroundColor: "transparent"
+    },
+    "& .rc-slider-rail": {
+      height: "10px"
+    },
+    "& .rc-slider-handle, .rc-slider-handle:hover, .rc-slider-handle:focus, .rc-slider-handle:active": {
+      backgroundColor: "blue",
+      outline: "none",
+      boxShadow: "none",
+      border: "2px solid blue",
+      width: "13px",
+      height: "13px",
+      marginLeft: "-6px",
+      marginTop: "-2px",
+    }
+  },
+  selectContainer: {
+    marginLeft: "auto",
+    marginRight: "1rem"
+  }
+};
